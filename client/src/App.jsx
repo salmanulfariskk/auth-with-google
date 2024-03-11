770eca0f40c22589e3f2fd1723ed7f6065ec1c13
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-11 16:38:42 +0530

    cerate pages and set the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 function App() {
   return (
-    <div className="text-red-600">
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

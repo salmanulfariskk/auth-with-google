39803112fbb1602f088e2d3cae8896bb1b65da41
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-11 17:17:42 +0530

    header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>

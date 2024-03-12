0341815746b0edeebc18270a1a9ada35928e3844
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-12 12:32:28 +0530

    setup rotes and controller

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,6 +11,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express();
+
+app.use('/api/user',userRoutes )
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');

d7d97238e582850b951f297724cbcca8069cfcc0
SALMANULFARISKK <salmanulfaris094@gmail.com>
2024-03-16 14:37:08 +0530

    user update user api route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 dotenv.config()
+import cookieParser from 'cookie-parser';
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('database connected successfully');
@@ -13,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/user',userRoutes )
 app.use('/api/auth',authRoutes )
